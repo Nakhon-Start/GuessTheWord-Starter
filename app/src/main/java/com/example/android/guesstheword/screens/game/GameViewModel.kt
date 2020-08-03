5431b072e4a8648b5fd457dd420d52e0a030416b
Nakhon-Start <56624645+Nakhon-Start@users.noreply.github.com>
2020-08-03 21:16:56 +0700

    Android Kotlin Fundamentals 05.2: LiveData and LiveData observers
    6. Task: Encapsulate the LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -12,7 +13,11 @@ class GameViewModel : ViewModel() {
 
     val word = MutableLiveData<String>()
     // The current score
-    val score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -50,7 +55,7 @@ class GameViewModel : ViewModel() {
     init {
         Log.i("GameViewModel", "GameViewModel created!")
         word.value = ""
-        score.value = 0
+        _score.value = 0
         resetList()
         nextWord()
     }
@@ -65,11 +70,11 @@ class GameViewModel : ViewModel() {
 
     /** Methods for updating the UI **/
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
